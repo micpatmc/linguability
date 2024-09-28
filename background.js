94404caa556aec9de4e3f9e07634e0ba6f3504b7
Michael McGarvey <71113885+MochaGamesDev@users.noreply.github.com>
2024-09-28 04:54:13 -0400

    Implement functionality to replace all words on a page

---
+++ background.js
@@ -0,0 +1,13 @@
+// Script to execute the content.js file when a tab is loaded
+try{
+  chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+    if(changeInfo.status == 'complete'){
+      chrome.scripting.executeScript({
+        files: ['content.js'],
+        target: {tabId: tab.id}
+      });
+    }
+  });
+}catch(e){
+  console.log(e);
+}
